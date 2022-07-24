1f4c708d3f7c2aadc6586ecc26e27886a0556638
manoranjan001p <95644446+manoranjan001p@users.noreply.github.com>
2022-07-24 23:29:07 +0530

    Create reverselist.py

---
+++ reverselist.py
@@ -0,0 +1,14 @@
+lst = [10, 11, 12, 13, 14, 15]
+# the above input can also be given as
+# lst=list(map(int,input().split()))
+l = []  # empty list
+lst.sort(reverse=True)
+print(lst)
+# checking if elements present in the list or not
+for i in lst:
+    print(i)
+    # reversing the list
+    l.insert(0, i)
+    print(l)
+# printing result
+print(l)
