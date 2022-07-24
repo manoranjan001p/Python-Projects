647d7e25af82cc8a4a835f78649ce8ded2df0943
manoranjan001p <95644446+manoranjan001p@users.noreply.github.com>
2022-07-24 23:51:25 +0530

    Update reverselist.py

--- reverselist.py
+++ reverselist.py
@@ -2,7 +2,7 @@ lst = [10, 11, 12, 13, 14, 15]
 # the above input can also be given as
 # lst=list(map(int,input().split()))
 l = []  # empty list
-lst.sort(reverse=True)
+lst.sort(reverse=True)  #lst=lst[::-1] for reversing the letter
 print(lst)
 # checking if elements present in the list or not
 for i in lst:
